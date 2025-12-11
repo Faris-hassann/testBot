126c9c0b7244d9766bf01ebae7d270fa716c1e41
Faris hassan <felmola13@gmail.com>
2025-12-11 12:25:59 +0200

    Update Settings.py

--- bitrix_bot/settings.py
+++ bitrix_bot/settings.py
@@ -121,7 +121,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Bitrix24 Configuration
 BITRIX_DOMAIN = os.environ.get('BITRIX_DOMAIN', 'cultiv.bitrix24.com')
-BITRIX_CLIENT_ID = os.environ.get('BITRIX_CLIENT_ID', 'local.68209f93737861.69630009')
-BITRIX_CLIENT_SECRET = os.environ.get('BITRIX_CLIENT_SECRET', 'le2Wpb7mRQ8pDRa1MmH0ZpkmSLyvrPz4sPD3rqJd28PsNhfF')
+BITRIX_CLIENT_ID = os.environ.get('BITRIX_CLIENT_ID', 'local.693a9c0f77abd5.76481022')
+BITRIX_CLIENT_SECRET = os.environ.get('BITRIX_CLIENT_SECRET', 'TiR9H0GMYCUcJOYu0eQhmveEUy7YacZUtBJkYE4AYEWOeiLhV2')
 BITRIX_EVENT_HANDLER = os.environ.get('BITRIX_EVENT_HANDLER', 'https://testbot-2x1r.onrender.com/b24-hook.php')
 
